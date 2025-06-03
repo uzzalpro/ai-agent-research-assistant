bf2a281554ab02db4be9ec545ca8970f7f71032b
pro <uzzal.dna@gmail.com>
2025-06-04 03:02:38 +0600

    add tools

---
+++ main.py
@@ -0,0 +1,74 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+import os
+import json
+
+
+load_dotenv()
+API_KEY = os.getenv('GEMINI_API_KEY')
+# API_KEY = os.getenv('OPENAI_API_KEY')
+# llm = ChatOpenAI(model="gpt-3.5-turbo",)
+# llm = ChatAnthropic(model="Claude Opus 4")
+
+
+# response = llm.invoke("What is the meaning of life?")
+# print(response)
+
+# AIzaSyCKrvt-az5HanGNuvf8PPUVqvJnxsx2A8s
+# from google import genai
+
+# client = genai.Client(api_key="AIzaSyB5QMmKDfccbS1RhQwMa2DpkSvmv9PcRzA")
+
+# response = client.models.generate_content(
+#     model="gemini-2.0-flash", contents="What is the meaning of life?"
+# )
+# print(response.text)
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", api_key=API_KEY)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can i help you research? ")
+raw_response = agent_executor.invoke({"query": query})
+
+try:
+    structured_response = parser.parse(raw_response.get("output"))
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
